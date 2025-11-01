8506edc05459b3b3877d9ffc21c8b14c4f7d20ef
Kana Nagata <ngtkana@gmail.com>
2025-11-02 01:31:15 +0900

    add ptr.rs to explain a know problem

---
+++ examples/ptr.rs
@@ -0,0 +1,20 @@
+use procon_lg::lg_recur;
+
+#[lg_recur]
+fn countdown(#[no_debug] x: *const i32) {
+    unsafe {
+        if *x == 0 {
+            println!("Bang!");
+        } else {
+            let x = *x - 1;
+            let x = &x;
+            // countdown(x); <- compile error!
+            countdown(std::ptr::from_ref(x));
+        }
+    }
+}
+
+fn main() {
+    let x = 3;
+    countdown(&raw const x);
+}
